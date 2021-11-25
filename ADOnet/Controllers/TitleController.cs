0429244bc5cd1ea251028ef2190675c78d84bad0
Ivan Lanovskyi <31585738+ivanlanovsky@users.noreply.github.com>
2021-11-25 21:57:25 +0200

    details

---
+++ ADOnet/Controllers/TitleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryBLL.Services;
+using LibraryBLL.Dto;
+
+namespace ADOnet.Controllers
+{
+    public class TitleController : Controller
+    {
+        private readonly ITitleService tService;
+        public TitleController(ITitleService service)
+        {
+            this.tService = service;
+        }
+        [HttpGet]
+        public ActionResult Details(int id)
+        {
+            TitleViewModel title = tService.GetById(id);
+            if (title == null)
+                return View("NotFound");
+            return View(title);
+        }
+    }
+}
